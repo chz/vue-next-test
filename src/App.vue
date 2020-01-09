2e16a0f6bd044443b2cf74138b3a98774b25d6e6
Chingiz Mammadov <info@chz.dev>
2020-01-09 19:19:16 +0400

    Test

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>Test vue3</h1>
+  <div>
+    <div v-if="isVisible">
+      Is Visible?
+    </div>
+    <button @click="isVisible = !isVisible">Toggle Visibility</button>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+export default {
+  setup() {
+    let isVisible = ref(false)
+    return {
+      isVisible
+    }
+  }
+}
+</script>
